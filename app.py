d54ae030e189933a3cee7dc9171a4543f1351ddf
mopzera7 <marquesmatheus105@gmail.com>
2026-04-11 20:18:52 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -21,34 +21,44 @@ def calcular_ifr(series, periodos):
 @st.cache_data(ttl="1d")
 def varrer_mercado_ao_vivo():
     tickers = [
-        "AALR3", "ABCB4", "ABEV3", "AERI3", "AGRO3", "AGXY3", "ALLD3", "ALOS3", "ALPA4", "ALPK3",
-        "ALUP11", "AMAR3", "AMBP3", "AMER3", "AMOB3", "ANIM3", "ARML3", "ASAI3", "AURA33", "AURE3",
-        "AXIA3", "AXIA6", "AXIA7", "AZEV3", "AZEV4", "AZUL4", "AZZA3", "B3SA3", "BBAS3", "BBDC3",
-        "BBDC4", "BBSE3", "BEEF3", "BEES3", "BEES4", "BHIA3", "BIOM3", "BLAU3", "BMEB4", "BMGB4",
-        "BMOB3", "BOBR4", "BPAC11", "BPAN4", "BRAP3", "BRAP4", "BRAV3", "BRBI11", "BRKM5", "BRSR3",
-        "BRSR6", "CAMB3", "CAML3", "CASH3", "CBAV3", "CEAB3", "CGRA4", "CMIG3", "CMIG4", "CMIN3",
-        "COCE5", "COGN3", "CPFE3", "CPLE3", "CSAN3", "CSED3", "CSMG3", "CSNA3", "CSUD3", "CURY3",
-        "CVCB3", "CXSE3", "CYRE3", "DASA3", "DESK3", "DEXP3", "DIRR3", "DIVO11", "DMVF3", "DOTZ3",
-        "DXCO3", "EALT4", "ECOR3", "EGIE3", "EMBR3", "ENEV3", "ENGI11", "ENJU3", "EQTL3", "ESPA3",
-        "ETER3", "EUCA4", "EVEN3", "EZTC3", "FESA4", "FHER3", "FIQE3", "FLRY3", "FRAS3", "GFSA3",
-        "GGBR3", "GGBR4", "GGPS3", "GMAT3", "GOAU3", "GOAU4", "GOLD11", "GRND3", "GUAR3", "HAGA4",
-        "HAPV3", "HBOR3", "HBRE3", "HBSA3", "HYPE3", "IFCM3", "IGTI11", "IGTI3", "INEP3", "INEP4",
-        "INTB3", "IRBR3", "ISAE4", "ITSA3", "ITSA4", "ITUB3", "ITUB4", "JALL3", "JBSS32", "JHSF3",
-        "JSLG3", "KEPL3", "KLBN11", "KLBN3", "KLBN4", "LAND3", "LAVV3", "LEVE3", "LIGT3", "LJQQ3",
-        "LOGG3", "LOGN3", "LPSB3", "LREN3", "LUPA3", "LWSA3", "MATD3", "MBRF3", "MDIA3", "MDNE3",
-        "MEAL3", "MELK3", "MGLU3", "MILS3", "MLAS3", "MOTV3", "MOVI3", "MRVE3", "MTRE3", "MULT3",
-        "MYPK3", "NDIV11", "NEOE3", "NGRD3", "NSDV11", "ODPV3", "OFSA3", "OIBR3", "OIBR4", "ONCO3",
-        "OPCT3", "ORVR3", "PCAR3", "PDGR3", "PDTC3", "PETR3", "PETR4", "PETZ3", "PFRM3", "PGMN3",
-        "PINE4", "PLPL3", "PMAM3", "PNVL3", "POMO3", "POMO4", "PORT3", "POSI3", "PRIO3", "PRNR3",
-        "PSSA3", "PTBL3", "PTNT4", "QUAL3", "RADL3", "RAIL3", "RAIZ4", "RANI3", "RAPT4", "RCSL3",
-        "RCSL4", "RDOR3", "RECV3", "RENT3", "RIAA3", "RNEW11", "RNEW3", "ROMI3", "RSID3", "SANB11",
-        "SANB3", "SANB4", "SAPR11", "SAPR3", "SAPR4", "SBFG3", "SBSP3", "SCAR3", "SEER3", "SEQL3",
-        "SHOW3", "SHUL4", "SIMH3", "SLCE3", "SMFT3", "SMTO3", "SOJA3", "SRNA3", "SUZB3", "SYNE3",
-        "TAEE11", "TAEE3", "TAEE4", "TASA3", "TASA4", "TCSA3", "TECN3", "TEND3", "TFCO4", "TGMA3",
-        "TIMS3", "TOTS3", "TPIS3", "TRAD3", "TRIS3", "TTEN3", "TUPY3", "UCAS3", "UGPA3", "UNIP6",
-        "USIM3", "USIM5", "USTK11", "VALE3", "VAMO3", "VBBR3", "VGIA11", "VITT3", "VIVA3", "VIVR3",
-        "VIVT3", "VLID3", "VSTE3", "VULC3", "VVEO3", "WEB311", "WEGE3", "WEST3", "WHRL4", "WIZC3",
-        "XPBR31", "YDUQ3",
+        "A1MD34", "AALR3", "AAPL34", "ABBV34", "ABCB4", "ABEV3", "ABTT34", "ADBE34", "AERI3", "AGRO3",
+        "AGXY3", "ALLD3", "ALOS3", "ALPA4", "ALPK3", "ALUP11", "AMAR3", "AMBP3", "AMER3", "AMGN34",
+        "AMOB3", "AMZO34", "ANIM3", "ARML3", "ASAI3", "ASML34", "ATMP3", "ATTB34", "AUAU3", "AURA33",
+        "AURE3", "AVGO34", "AXIA3", "AXIA6", "AXIA7", "AXPB34", "AZEV3", "AZEV4", "AZUL4", "AZZA3",
+        "B1IL34", "B3SA3", "BABA34", "BBAS3", "BBDC3", "BBDC4", "BBSE3", "BCSA34", "BEEF3", "BEES3",
+        "BEES4", "BERK34", "BHIA3", "BIDU34", "BIOM3", "BKNG34", "BLAK34", "BLAU3", "BMEB4", "BMGB4",
+        "BMOB3", "BOAC34", "BOBR4", "BOVA11", "BPAC11", "BRAP3", "BRAP4", "BRAV3", "BRBI11", "BRFS3",
+        "BRKM5", "BRSR3", "BRSR6", "C2OI34", "C2RW34", "CAMB3", "CAML3", "CASH3", "CATP34", "CBAV3",
+        "CCRO3", "CEAB3", "CEBR3", "CGRA4", "CHCM34", "CHVX34", "CLSA3", "CMCS34", "CMIG3", "CMIG4",
+        "CMIN3", "COCA34", "COCE5", "COGN3", "COPH34", "COWC34", "CPFE3", "CPLE3", "CSAN3", "CSCO34",
+        "CSED3", "CSMG3", "CSNA3", "CSUD3", "CTGP34", "CURY3", "CVCB3", "CXSE3", "CYRE3", "DASA3",
+        "DEEC34", "DESK3", "DEXP3", "DHER34", "DIRR3", "DISB34", "DIVO11", "DMVF3", "DOTZ3", "DXCO3",
+        "E1CO34", "EALT4", "EBAY34", "ECOR3", "EGIE3", "EMBR3", "ENEV3", "ENGI11", "ENJU3", "EQTL3",
+        "ESPA3", "ETER3", "EUCA4", "EVEN3", "EXXO34", "EZTC3", "FESA4", "FHER3", "FIEI3", "FIQE3",
+        "FLRY3", "FRAS3", "GEOO34", "GFSA3", "GGBR3", "GGBR4", "GGPS3", "GMAT3", "GMCO34", "GOAU3",
+        "GOAU4", "GOGL34", "GOGL35", "GOLL54", "GOLD11", "GRND3", "GSGI34", "GUAR3", "HAGA4", "HAPV3",
+        "HBOR3", "HBRE3", "HBSA3", "HYPE3", "IFCM3", "IGTI11", "IGTI3", "INEP3", "INEP4", "INTB3",
+        "INTU34", "IRBR3", "ISAE4", "ITLC34", "ITSA3", "ITSA4", "ITUB3", "ITUB4", "IVVB11", "JALL3",
+        "JBSS32", "JHSF3", "JNJB34", "JPMC34", "JSLG3", "K2CG34", "KEPL3", "KLBN11", "KLBN3", "KLBN4",
+        "KRSA3", "L1MN34", "LAND3", "LAVV3", "LEVE3", "LIGT3", "LILY34", "LJQQ3", "LOGG3", "LOGN3",
+        "LPSB3", "LREN3", "LUPA3", "LVTC3", "LWSA3", "M1RN34", "M1TA34", "MATD3", "MBLY3", "MCDC34",
+        "MDIA3", "MDNE3", "MEAL3", "MELI34", "MELK3", "MGLU3", "MILS3", "MLAS3", "MOOO34", "MOTV3",
+        "MOVI3", "MRCK34", "MRFG3", "MRVE3", "MSBR34", "MSCD34", "MSFT34", "MTRE3", "MULT3", "MUTC34",
+        "MYPK3", "N1EM34", "N1OW34", "N1VO34", "NASD11", "NATU3", "NDIV11", "NEOE3", "NEXT34", "NFLX34",
+        "NGRD3", "NIKE34", "NSDV11", "NVDC34", "ODPV3", "OFSA3", "OIBR3", "OIBR4", "ONCO3", "OPCT3",
+        "ORCL34", "ORVR3", "OXYP34", "P1DD34", "P2LT34", "PAGS34", "PCAR3", "PDGR3", "PDTC3", "PETR3",
+        "PETR4", "PETZ3", "PFIZ34", "PFRM3", "PGCO34", "PGMN3", "PINE4", "PLPL3", "PMAM3", "PNVL3",
+        "POMO3", "POMO4", "POSI3", "PRIO3", "PRNR3", "PSSA3", "PTBL3", "PTNT4", "QCOM34", "QUAL3",
+        "RADL3", "RAIL3", "RAIZ4", "RANI3", "RAPT4", "RCSL3", "RCSL4", "RDOR3", "RECV3", "RENT3",
+        "RIAA3", "RNEW11", "RNEW3", "ROMI3", "ROXO34", "RSID3", "S1PO34", "S2EA34", "S2HO34", "S2NW34",
+        "SANB11", "SANB3", "SANB4", "SAPR11", "SAPR3", "SAPR4", "SBFG3", "SBSP3", "SCAR3", "SCHW34",
+        "SEER3", "SEQL3", "SHOW3", "SHUL4", "SIMH3", "SIMN34", "SLCE3", "SMFT3", "SMTO3", "SOJA3",
+        "SPGI34", "SSFO34", "STBP3", "SUZB3", "SYNE3", "TAEE11", "TAEE3", "TAEE4", "TASA3", "TASA4",
+        "TCSA3", "TECN3", "TEND3", "TFCO4", "TGMA3", "TIMS3", "TOTS3", "TPIS3", "TRAD3", "TRIS3",
+        "TSLA34", "TSMC34", "TTEN3", "TUPY3", "U1BE34", "U1RI34", "UCAS3", "UGPA3", "UNHH34", "UNIP6",
+        "UPSS34", "USIM3", "USIM5", "USTK11", "VALE3", "VAMO3", "VBBR3", "VERZ34", "VGIA11", "VISA34",
+        "VITT3", "VIVA3", "VIVR3", "VIVT3", "VLID3", "VSTE3", "VTRU3", "VULC3", "VVEO3", "WALM34",
+        "WEB311", "WEGE3", "WEST3", "WFCO34", "WHRL4", "WIZC3", "XFIX11", "XPBR31", "YDUQ3"
     ]
     tickers_sa = [t + ".SA" for t in tickers]
     benchmarks = ["BOVA11.SA", "IVVB11.SA"]
@@ -83,9 +93,8 @@ def varrer_mercado_ao_vivo():
                 'Quantidade': dados_brutos['Volume'][t_sa]
             }).ffill().dropna()
 
-            # CORREÇÃO 1: Proteção absoluta contra IPOs recentes (evita erro de NaN na Max_52W)
             if len(df) < 252: continue 
-            
+        
             ticker_puro = t_sa.replace(".SA", "")
             
             df['MM20'] = df['Fechamento'].rolling(window=20).mean()
@@ -93,9 +102,9 @@ def varrer_mercado_ao_vivo():
             df['MM80'] = df['Fechamento'].rolling(window=80).mean()
             df['MM100'] = df['Fechamento'].rolling(window=100).mean()
             df['MM150'] = df['Fechamento'].rolling(window=150).mean()
-            df['VolMedio_20d'] = df['Quantidade'].rolling(window=20).mean()
-            df['VolMedio_60d'] = df['Quantidade'].rolling(window=60).mean()
-            df['VolMedio_100d'] = df['Quantidade'].rolling(window=100).mean()
+            df['QtdMedia_20d'] = df['Quantidade'].rolling(window=20).mean()
+            df['QtdMedia_60d'] = df['Quantidade'].rolling(window=60).mean()
+            df['QtdMedia_100d'] = df['Quantidade'].rolling(window=100).mean()
             df['IFR40'] = calcular_ifr(df['Fechamento'], periodos=40)
             df['IFR3'] = calcular_ifr(df['Fechamento'], periodos=3)
             df['Max_52W'] = df['Máximo'].rolling(window=252).max()
@@ -104,8 +113,9 @@ def varrer_mercado_ao_vivo():
 
             min_8 = df['Mínimo'].rolling(window=8).min()
             max_8 = df['Máximo'].rolling(window=8).max()
-            k_rapido = 100 * ((df['Fechamento'] - min_8) / (max_8 - min_8))
-            df['Estocastico_Lento'] = k_rapido.rolling(window=3).mean()
+            denominador = (max_8 - min_8).replace(0, np.nan)
+            k_rapido = 100 * ((df['Fechamento'] - min_8) / denominador)
+            df['Estocastico_Lento'] = k_rapido.rolling(window=3).mean().fillna(50)
 
             ema_curta = df['Fechamento'].ewm(span=3, adjust=False).mean()
             ema_longa = df['Fechamento'].ewm(span=10, adjust=False).mean()
@@ -124,7 +134,6 @@ def varrer_mercado_ao_vivo():
             df_ano_atual = df[df.index.year == ano_atual]
             retorno_ano = (preco_atual / df_ano_atual.iloc[0]['Fechamento']) - 1 if not df_ano_atual.empty else 0
             
-            # CORREÇÃO 3: Blindagem de divisão por zero ajustada (-1 significa queda de 100%)
             fr_ibov = ((1 + retorno_12m) / (1 + retorno_12m_ibov)) - 1 if retorno_12m_ibov != -1 else 0
             fr_ivvb = ((1 + retorno_12m) / (1 + retorno_12m_ivvb)) - 1 if retorno_12m_ivvb != -1 else 0
 
@@ -135,8 +144,8 @@ def varrer_mercado_ao_vivo():
                 'MACD_Linha_10_3': hoje['MACD_Linha'], 'MACD_Media_36': hoje['MACD_Media_36'],
                 'Max_52W': hoje['Max_52W'], 'Min_52W': hoje['Min_52W'], 'Topo_Historico': hoje['Topo_Historico'],
                 'MM20': hoje['MM20'], 'MM50': hoje['MM50'], 'MM80': hoje['MM80'],
-                'MM100': hoje['MM100'], 'MM150': hoje['MM150'], 'Volume_Acoes': hoje['Quantidade'],
-                'VolMedio_20d': hoje['VolMedio_20d'], 'VolMedio_60d': hoje['VolMedio_60d'], 'VolMedio_100d': hoje['VolMedio_100d']
+                'MM100': hoje['MM100'], 'MM150': hoje['MM150'], 'Qtd_Acoes': hoje['Quantidade'],
+                'QtdMedia_20d': hoje['QtdMedia_20d'], 'QtdMedia_60d': hoje['QtdMedia_60d'], 'QtdMedia_100d': hoje['QtdMedia_100d']
             })
         except Exception as e:
             pass
@@ -255,14 +264,13 @@ with st.sidebar.expander("🏆 Filtros de Performance"):
     retorno_12m_minimo = st.number_input("Retorno 12M Mínimo (%)", value=-100.0, step=10.0)
 
 with st.sidebar.expander("💰 Filtros de Liquidez"):
-    # CORREÇÃO 2: Atualização de Nomenclatura para refletir Volume de Ações reais
-    filtro_negocios = st.number_input("Volume de Ações Hoje (Mínimo)", value=10000, step=10000)
-    filtro_qtdmm20 = st.number_input("Volume Médio 20d (Mínimo)", value=0, step=10000)
-    volume_crescente = st.checkbox("Liquidez Crescente (Vol. 20d > Vol. 60d)", value=False)
+    filtro_negocios = st.number_input("Qtd. de Ações Hoje (Mínimo)", value=10000, step=10000)
+    filtro_qtdmm20 = st.number_input("Qtd. Média 20d (Mínimo)", value=0, step=10000)
+    volume_crescente = st.checkbox("Liquidez Crescente (Qtd. 20d > Qtd. 60d)", value=False)
 
 st.sidebar.markdown("---")
 ordenar_por = st.sidebar.selectbox("Ordenar Resultados por:", 
-    ["FR_IBOV", "Retorno_12M", "IFR40", "MACD_Linha_10_3", "Volume_Acoes"])
+    ["FR_IBOV", "Retorno_12M", "IFR40", "MACD_Linha_10_3", "Qtd_Acoes"])
 
 # ==========================================
 # 4. EXECUÇÃO E APRESENTAÇÃO
@@ -277,8 +285,8 @@ with st.spinner("Analisando o mercado e aplicando os filtros em tempo real..."):
 
         t = t[
             (t['Fechamento'] >= preco_minimo) &
-            (t['Volume_Acoes'] >= filtro_negocios) &
-            (t['VolMedio_20d'] >= filtro_qtdmm20) &
+            (t['Qtd_Acoes'] >= filtro_negocios) &
+            (t['QtdMedia_20d'] >= filtro_qtdmm20) &
             (t['Retorno_12M'] >= (retorno_12m_minimo / 100)) &
             (t['FR_IBOV'] >= (fr_ibov_minimo / 100)) &
             (t['FR_IVVB'] >= (fr_ivvb_minimo / 100))
@@ -315,7 +323,7 @@ with st.spinner("Analisando o mercado e aplicando os filtros em tempo real..."):
             if tend_mm80_150: t = t[t['MM80'] < t['MM150']]
             if rompimento_52w: t = t[t['Fechamento'] <= (t['Min_52W'] * 1.05)]
 
-        if volume_crescente: t = t[t['VolMedio_20d'] > t['VolMedio_60d']]
+        if volume_crescente: t = t[t['QtdMedia_20d'] > t['QtdMedia_60d']]
 
         t = t.sort_values(by=ordenar_por, ascending=False).reset_index(drop=True)
 
@@ -325,7 +333,7 @@ with st.spinner("Analisando o mercado e aplicando os filtros em tempo real..."):
             'Retorno_Ano', 'Retorno_12M', 'FR_IBOV', 'FR_IVVB', 
             'IFR3', 'IFR40', 'Estocastico_Lento', 'MACD_Linha_10_3', 'MACD_Media_36', 
             'MM20', 'MM50', 'MM80', 'MM100', 'MM150', 
-            'Volume_Acoes', 'VolMedio_20d', 'VolMedio_60d', 'VolMedio_100d'
+            'Qtd_Acoes', 'QtdMedia_20d', 'QtdMedia_60d', 'QtdMedia_100d'
         ]
         t = t[ordem_colunas] 
 
@@ -342,13 +350,9 @@ with st.spinner("Analisando o mercado e aplicando os filtros em tempo real..."):
         colunas_2dec = ['IFR3', 'IFR40', 'Estocastico_Lento']
         for col in colunas_2dec: t[col] = t[col].apply(lambda x: f"{x:.2f}")
 
-        colunas_int = ['Volume_Acoes', 'VolMedio_20d', 'VolMedio_60d', 'VolMedio_100d']
+        colunas_int = ['Qtd_Acoes', 'QtdMedia_20d', 'QtdMedia_60d', 'QtdMedia_100d']
         for col in colunas_int: t[col] = t[col].apply(lambda x: f"{x:,.0f}".replace(',', '.'))
 
         # VISUALIZAÇÃO FOCADA (CLEAN UI)
         st.success(f"Sincronizado! {len(t)} ações passaram nos filtros.")
         st.dataframe(t, use_container_width=True, height=750)
-
-
-
-
